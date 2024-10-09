c7ea88a12b8f6a19bc66ad819fbea9413fc017a7
Coslate <coslate@media.ee.ntu.edu.tw>
2024-10-09 05:36:25 +0000

    Complete HW4.

--- HW4/handout/lr.py
+++ HW4/handout/lr.py
@@ -2,7 +2,8 @@ import numpy as np
 import argparse
 import csv
 from numpy.typing import NDArray
-from typing import Tuple, Any
+from typing import Tuple, Any, List
+import matplotlib.pyplot as plt
 
 def load_tsv_dataset(file):
     """
@@ -40,13 +41,23 @@ def sigmoid(x : np.ndarray):
     e = np.exp(x)
     return e / (1 + e)
 
+def calNLL(X: np.ndarray, y: np.ndarray, theta: np.ndarray) -> np.float32:
+    nll = 0.0
+    for i in range(X.shape[0]):
+        dot_val = np.dot(theta, X[i])
+        nll += y[i]*dot_val - np.log(1+np.exp(dot_val))
+    return nll*-1.0/np.float32(X.shape[0])
+
 
 def train(
     theta : np.ndarray, # shape (D,) where D is feature dim
     X : np.ndarray,     # shape (N, D) where N is num of examples
     y : np.ndarray,     # shape (N,)
     num_epoch : int, 
-    learning_rate : float
+    learning_rate : float,
+    val_X: np.ndarray,
+    val_y: np.ndarray,
+    plot_epoch_num: int
 ) -> None:
     # TODO: Implement `train` using vectorization
     # Folded intercept into theta: D+1 dimension
@@ -54,7 +65,13 @@ def train(
 
     # Add 1 to each X for intercept term
     ones_column = np.ones((X.shape[0], 1))
+    ones_column_val = np.ones((val_X.shape[0], 1))
     X_intercept = np.concatenate((ones_column, X), axis=1)
+    val_X_intercept = np.concatenate((ones_column_val, val_X), axis=1)
+    nll_train = 0
+    nll_val   = 0
+    avgnll_train = []
+    avgnll_val   = []
     
     # SGD
     for num in range(num_epoch):
@@ -63,7 +80,12 @@ def train(
             gradient = X_intercept[i]*(sigmoid(dot_val)-y[i])
             theta_include_intercept -= learning_rate*gradient
 
-    return theta_include_intercept
+        # Calculate Average Negative Loglikelihood in train & val
+        if num < plot_epoch_num:
+            avgnll_train.append(calNLL(X_intercept, y, theta_include_intercept))
+            avgnll_val.append(calNLL(val_X_intercept, val_y, theta_include_intercept))
+
+    return theta_include_intercept, avgnll_train, avgnll_val
 
 
 def hx(x: np.ndarray, theta: np.ndarray) -> [[]]:
@@ -112,6 +134,53 @@ def writeOutMetrix(output_file: str, error_train: float, error_test: float) -> N
         f.write(f"error(train): {error_train:.6f}\n")
         f.write(f"error(test): {error_test:.6f}\n")
 
+def plotFigureAvgNLL_LearningRate(avgnll_train_lr1: List[Any], avgnll_train_lr2: List[Any], avgnll_train_lr3: List[Any], plot_epoch_num: int):
+    epochs = list(range(0, plot_epoch_num))
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(epochs, avgnll_train_lr1, label='Training w/ lr=0.1 Avg Negative Log-likelihood', linestyle='-', linewidth=2)
+    plt.plot(epochs, avgnll_train_lr2, label='Training w/ lr=0.01 Avg Negative Log-likelihood', linestyle='--', linewidth=2)
+    plt.plot(epochs, avgnll_train_lr3, label='Training w/ lr=0.001 Avg Negative Log-likelihood', linestyle=':', linewidth=2)
+
+    # Add title
+    plt.xlabel('Epochs')
+    plt.ylabel('Avg Negative Log-likelihood')
+    plt.title('Training Average Negative Log-Likelihood w/ Different lr over Epochs')
+    plt.legend()
+
+    # Show the grid for better readability
+    plt.grid(True)
+
+    # Save the image
+    plt.savefig('Avg_NLL_Training_w_Diff_lr_over_Epochs.png', dpi=300, bbox_inches='tight')
+
+    # Display the plot
+    plt.show()
+
+def plotFigureAvgNLL(avgnll_train: List[Any], avgnll_val: List[Any], plot_epoch_num: int):
+    epochs = list(range(0, plot_epoch_num))
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(epochs, avgnll_train, label='Training Avg Negative Log-likelihood', linestyle='-', linewidth=2)
+    plt.plot(epochs, avgnll_val, label='Validation Avg Negative Log-likelihood', linestyle='--', linewidth=2)
+
+    # Add title
+    plt.xlabel('Epochs')
+    plt.ylabel('Avg Negative Log-likelihood')
+    plt.title('Training and Validation Average Negative Log-Likelihood over Epochs')
+    plt.legend()
+
+    # Show the grid for better readability
+    plt.grid(True)
+
+    # Save the image
+    plt.savefig('Avg_NLL_over_Epochs.png', dpi=300, bbox_inches='tight')
+
+    # Display the plot
+    plt.show()
+
+
+
 if __name__ == '__main__':
     # This takes care of command line argument parsing for you!
     # To access a specific argument, simply access args.<argument name>.
@@ -129,10 +198,16 @@ if __name__ == '__main__':
                         help='learning rate for stochastic gradient descent')
     parser.add_argument("--is_debug", type=int, default=0,
                         help='show debug msg.')
+    parser.add_argument("--plotfig", type=int, default=0,
+                        help='show figure for Avg NLL vs epoches for train & val dataset.')
+    parser.add_argument("--plot_epoch_num", type=int, default=50,
+                        help='the epoch number to plot train/val Avg NLL over epochs')
+    parser.add_argument("--plot_epoch_num_lr", type=int, default=50,
+                        help='the epoch number to plot different lr of Avg NLL over epochs.')
     args = parser.parse_args()
 
     train_input           = args.train_input
-    validation_input      = args.validation_input
+    val_input             = args.validation_input
     test_input            = args.test_input
     train_out             = args.train_out
     test_out              = args.test_out
@@ -140,6 +215,11 @@ if __name__ == '__main__':
     num_epoch             = args.num_epoch
     learning_rate         = args.learning_rate
     is_debug              = args.is_debug
+    plotfig               = args.plotfig
+    plot_epoch_num        = args.plot_epoch_num
+    plot_epoch_num_lr     = args.plot_epoch_num_lr
+    plot_epoch_num = plot_epoch_num if num_epoch > plot_epoch_num else num_epoch
+    plot_epoch_num_lr = plot_epoch_num_lr if num_epoch > plot_epoch_num_lr else num_epoch
 
     # Loading
     if is_debug: print(f"> load_tsv_dataset()...")
@@ -149,11 +229,20 @@ if __name__ == '__main__':
     test_X, test_y = load_tsv_dataset(test_input)
     test_X = np.array(test_X)
     test_y = np.array(test_y)
+    val_X, val_y = load_tsv_dataset(val_input)
+    val_X = np.array(val_X)
+    val_y = np.array(val_y)
 
     # Training
     if is_debug: print(f"> train()...")
+    lr1 = 0.1
+    lr2 = 0.01
+    lr3 = 0.001
     theta_initial = np.zeros(train_X.shape[1])
-    theta_trained = train(theta_initial, train_X, train_y, num_epoch, learning_rate)
+    theta_trained, avgnll_train, avgnll_val = train(theta_initial, train_X, train_y, num_epoch, learning_rate, val_X, val_y, plot_epoch_num)
+    theta_trained_lr1, avgnll_train_lr1, avgnll_val_lr1 = train(theta_initial, train_X, train_y, num_epoch, lr1, val_X, val_y, plot_epoch_num_lr)
+    theta_trained_lr2, avgnll_train_lr2, avgnll_val_lr2 = train(theta_initial, train_X, train_y, num_epoch, lr2, val_X, val_y, plot_epoch_num_lr)
+    theta_trained_lr3, avgnll_train_lr3, avgnll_val_lr3 = train(theta_initial, train_X, train_y, num_epoch, lr3, val_X, val_y, plot_epoch_num_lr)
 
     # Predicting
     if is_debug: print(f"> predict()...")
@@ -170,3 +259,8 @@ if __name__ == '__main__':
     writeOutFile(train_out, predict_train_x)
     writeOutFile(test_out, predict_test_x)
     writeOutMetrix(metrics_out, error_train, error_test)
+
+    # PlotFigure
+    if plotfig == 1:
+        plotFigureAvgNLL(avgnll_train, avgnll_val, plot_epoch_num)
+        plotFigureAvgNLL_LearningRate(avgnll_train_lr1, avgnll_train_lr2, avgnll_train_lr3, plot_epoch_num_lr)
